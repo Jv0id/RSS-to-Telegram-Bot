c7f6495357c633768d5d112337f94b968b7b84bd
jiangpeng <jiangp@fusionskye.com>
2024-08-12 10:56:35 +0800

    update 08-12-2024 version:2.8.1

--- src/aio_helper.py
+++ src/aio_helper.py
@@ -1,21 +1,6 @@
 """
 Asyncio helper functions.
 """
-#  RSS to Telegram Bot
-#  Copyright (C) 2022-2024  Rongrong <i@rong.moe>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import annotations
 from typing import Callable, Optional
@@ -37,7 +22,7 @@ CPU_COUNT = os.cpu_count()
 AVAIL_CPU_COUNT = len(os.sched_getaffinity(0)) if hasattr(os, 'sched_getaffinity') else CPU_COUNT
 PROCESS_COUNT = min(AVAIL_CPU_COUNT, 3) if env.MULTIPROCESSING else 1
 
-THREAD_POOL_WEIGHT = 1
+THREAD_POOL_WEIGHT = 2
 PROCESS_POOL_WEIGHT = PROCESS_COUNT - 1
 
 POOL_TYPE = Literal['thread', 'process']
